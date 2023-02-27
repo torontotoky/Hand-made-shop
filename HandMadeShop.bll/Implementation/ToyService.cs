b6254e59d4ce2d5795c33709832fe5ffbfe64de2
torontotoky <nickolai.bel1aev@yandex.ru>
2023-02-27 18:06:01 +0300

    Сделал сервис для игрушек, осталось для материала и покупки

---
+++ HandMadeShop.bll/Implementation/ToyService.cs
@@ -0,0 +1,67 @@
+﻿using AutoMapper;
+using HandMadeShop.bll.Interfaces;
+using HandMadeShop.bll.models;
+using HandMadeShop.dal.context;
+using HandMadeShop.dal.entites;
+
+namespace HandMadeShop.bll.Implementation
+{
+    public class ToyService : ITouService
+    {
+        private readonly ApplicationContext db;
+        private readonly IMapper mapper;
+
+        public ToyService(ApplicationContext db, IMapper mapper)
+        {
+            this.db = db;
+            this.mapper = mapper;
+        }
+
+        public async Task<BToy> CreateToy(BToy bToy)
+        {
+            var newToy = await db.Toys.AddAsync(this.mapper.Map<Toy>(bToy));
+            return this.mapper.Map<BToy>(newToy);
+        }
+
+        public void DeleteToy(int id)
+        {
+            var toy = db.Toys.SingleOrDefault(x => x.Id == id);
+
+            if (toy == null)
+            {
+                throw new Exception($"Buheirf с таким {id} не найдена");
+            }
+
+            db.Toys.Remove(toy);
+        }
+
+        public BToy GetToy(int id)
+        {
+            var toy = db.Toys.SingleOrDefault(x => x.Id == id);
+            return this.mapper.Map<BToy>(toy);
+        }
+
+        public IEnumerable<BToy> GetToys()
+        {
+            return this.mapper.Map<IEnumerable<BToy>>(db.Toys);
+        }
+
+        public BToy UpdateToy(BToy bToy)
+        {
+            var toy = db.Toys.SingleOrDefault(x => x.Id == bToy.Id);
+
+            if (toy == null)
+            {
+                throw new Exception($"Игрушка с таким {bToy.Id} не найдена");
+            }
+
+            toy.Size = bToy.Size;
+            toy.Discription = bToy.Discription;
+            toy.Name = bToy.Name;
+
+            db.Toys.Update(toy);
+
+            return bToy;
+        }
+    }
+}
---
+++ HandMadeShop.bll/Interfaces/ITouService.cs
@@ -0,0 +1,13 @@
+﻿using HandMadeShop.bll.models;
+
+namespace HandMadeShop.bll.Interfaces
+{
+    public interface ITouService
+    {
+        public Task<BToy> CreateToy(BToy bToy);
+        public BToy UpdateToy(BToy bToy);
+        public IEnumerable<BToy> GetToys();
+        public BToy GetToy(int id);
+        public void DeleteToy(int id);
+    }
+}
